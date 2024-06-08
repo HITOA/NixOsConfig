fb01877aba74e000ce9816a55bdf882256a9b30f
HITOA <charli.poirier@gmail.com>
2024-06-09 00:31:07 +0200

    Generate hardware config script

--- tools/generate-hardware-config.sh
+++ tools/generate-hardware-config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-hardware-config = $(nixos-generate-config --show-hardware-config)
+hardware_config=$(nixos-generate-config --show-hardware-config)
 
-for dir in ./profiles/
+for dir in ./profiles/*
 do
-    echo $dir
+    echo "$hardware_config" >> "$dir/hardware-configuration.nix"
 done
