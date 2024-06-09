74e637c1bb282d10c4a4258aacc812f55432b2ba
HITOA <charli.poirier@gmail.com>
2024-06-09 16:08:32 +0200

    template configuration.nix

--- profiles/default/configuration.nix
+++ profiles/default/configuration.nix
@@ -1,1 +1,45 @@
-uwu
+{ config, lib, pkgs, ... }:
+{
+
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking.hostName = "HITO";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Paris";
+
+  i18n.defaultLocale = "fr_FR.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  services.xserver = {
+    layout = "fr";
+    xkbVariant = "";
+  };
+  console.keyMap = "fr";
+
+  users.users = {
+    HITO = {
+      isNormalUser = true;
+      extraGroups = [ "networkmanager" "wheel" ];
+    };
+  };
+
+  system.stateVersion = "24.05";
+}
