0f5c562e6da6c1fbf1d19af076104b206f191530
HITOA <charli.poirier@gmail.com>
2024-06-12 01:28:15 +0200

    test

--- homemanagerModules/applications/terminal/zellij.nix
+++ homemanagerModules/applications/terminal/zellij.nix
@@ -2,16 +2,12 @@
 {
   options.applications.zellij = {
     enable = lib.mkEnableOption "Enable zellij terminal emulator.";
+
   };
 
   config = lib.mkIf config.applications.zellij.enable {
     programs.zellij = {
       enable = true;
-    };
-
-    applications.terminal = {
-      #enable = true;
-      #binary = "${pkgs.zellij}/bin/zellij";
     };
   };
 }
