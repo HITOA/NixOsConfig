9638e2ed07b7b42fc17a1878a0d987edc269b5e9
HITOA <charli.poirier@gmail.com>
2024-06-12 02:34:08 +0200

    test

--- homemanagerModules/default.nix
+++ homemanagerModules/default.nix
@@ -5,5 +5,7 @@
     ./applications/applications.nix
     ./applications/terminal/kitty.nix
     ./applications/terminal/zellij.nix
+    ./applications/ide/codium.nix
+    ./applications/wofi.nix
   ];
 }
