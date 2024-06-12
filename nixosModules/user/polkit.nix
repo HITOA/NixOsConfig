5e0f05efc926e2e54ac149a54c12bb9054f29363
HITOA <charli.poirier@gmail.com>
2024-06-12 03:53:01 +0200

    test

--- nixosModules/user/polkit.nix
+++ nixosModules/user/polkit.nix
@@ -6,17 +6,17 @@
 
   config = lib.mkIf config.polkit.enable {
     environment.systemPackages = [
-      pkgs.kdePackages.polkit-kde-agent-1
+      pkgs.polkit_gnome
     ];
 
-    systemd.user.services.polkit-agent = {
+    systemd.user.services.polkit-gnome-authentication-agent-1 = {
       description = "Polkit agent";
       wantedBy = [ "graphical-session.target" ];
       wants = [ "graphical-session.target" ];
       after = [ "graphical-session.target" ];
       serviceConfig = {
         Type = "simple";
-        ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
       };
     };
   };
