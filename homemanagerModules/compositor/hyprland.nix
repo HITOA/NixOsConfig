dd8856ed99813c9a2198c69aa3efb5e1857c8a3e
HITOA <charli.poirier@gmail.com>
2024-06-12 02:28:02 +0200

    test

--- homemanagerModules/compositor/hyprland.nix
+++ homemanagerModules/compositor/hyprland.nix
@@ -25,7 +25,8 @@
       bind = [
         ",F11,togglespecialworkspace"
       ] ++
-        lib.optional config.applications.terminal.enable "${config.hyprland.binding.mainMod}, Q, exec, ${config.applications.terminal.binary}";
+        lib.optional config.applications.terminal.enable "${config.hyprland.binding.mainMod}, Q, exec, ${config.applications.terminal.binary}" ++
+        lib.optional config.applications.wofi.enable "${config.hyprland.binding.mainMod}, SPACE, exec, wofi --show drun";
 
       exec-once = [
         "[workspace special; size 100% 100%;move 12.5% 40] ${config.applications.terminal.binary} ${config.applications.zellij.binary}"
